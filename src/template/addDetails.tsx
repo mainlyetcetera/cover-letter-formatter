269d0d39ae5e635228e92c42d8a8999f6b4171e1
mainlyetcetera <ericcampbell294@gmail.com>
2021-05-11 15:48:35 -0600

    Format template

--- src/template/addDetails.tsx
+++ src/template/addDetails.tsx
@@ -1,4 +1,23 @@
-function addDetails() {
+import * as types from '../types/types'
+
+function addDetails(details: types.data): string {
+  const { date, position, company, why, specifics, numValues, values, primeValue } = details
+  const result = `${date}
+
+Dear ${company},
+
+My name is Eric Campbell. I am applying for your opening for a ${position}. ${why}!
+
+I feel I can add value to ${company} as I have also built several full stack apps in the past. The simplest I have done was a ToDo app utilizing React, Express and PostgreSql. I prioritized making sure that it was updating live, so any change to the server would update the DOM; that needed React hooks lifecycle methods to achieve properly. Also, ${specifics}!
+
+But the real reason I want to work with your company is what you have shown in your ${numValues} values: ${values}! I particularly appreciate ${primeValue}! 
+
+I look forward to moving through the interview process and getting to know you all better!
+
+Sincerely,
+Eric Campbell, your next future dev!
+`
+  return result
 }
 
 export default addDetails
--- src/template/template.tsx
+++ src/template/template.tsx
@@ -1,3 +1,17 @@
-const template: string = 'test template'
+const template = `May 11, 2021
+
+Dear Awesome Atlassian People,
+
+My name is Eric Campbell. I am applying for your opening for a Full Stack Engineer working on Trello. This is the perfect opportunity to continue expanding my full stack skills while working on a product I am familiar with and a team I have respected for years!
+
+I feel I can add value to your company as I have used Trello extensively! I know its quirks in terms of keyboard shortcuts and how you can already effectively move cards between boards! I have enjoyed connecting Trello to tools like Zapier to integrate Trello with GitHub Issues! I have also built several full stack apps in the past. The simplest I have done was a ToDo app utilizing React, Express and PostgreSql. I prioritized making sure that it was updating live, so any change to the server would update the DOM; that needed React hooks lifecycle methods to achieve properly.
+
+But the real reason I want to work with your company is what you have shown in your 5 values: a commitment to doing right by your people, both your devs and your customers! I particularly appreciate "Open company, no bullshit" for encouraging whole-company collaboration and "Don't #@!% the customer" as it shows you remember that who we're building these products for! I also love the 1% pledge as a healthy commitment to giving back to your community!
+
+I look forward to moving through the interview process and getting to know you all better!
+
+Sincerely,
+Eric Campbell, your next future dev!
+`
 
 export default template
