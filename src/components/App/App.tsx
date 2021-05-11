cfde1bf6b935908ac9c92c73d6b09d099b471a60
mainlyetcetera <ericcampbell294@gmail.com>
2021-05-11 11:32:43 -0600

    Move, update App

---
+++ src/components/App/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import Form from '../Form/Form'
+
+function App() {
+  return (
+    <div className="App">
+      App
+      <Form />
+    </div>
+  )
+}
+
+export default App
